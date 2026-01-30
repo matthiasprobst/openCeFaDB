02d4404ce8fff65ae0038a97de611f72abfdd4c8
MatthiasProbst <matth.probst@gmail.com>
2026-01-30 06:35:14 +0100

    add codemeta, citation.cff, changelog, cfd comparison notebook

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# CHANGELOG
+
+Log of changes in the versions
+
+## v1.0.1
+
+- Update to newer version of `h5rdmtoolbox` (v2.7.2).
+- added CITATION.cff file and codemeta.json for better citation support.
+- working directories of local RDF and HDF files is constructed based on the catalog version.
+- upgrade to `h5rdmtoolbox` 2.7.2
+
+## v1.0.0
+
+- Initial release of the project.
+- Added core features and functionalities.
--- tests/test_fan_curve.py
+++ tests/test_fan_curve.py
@@ -46,11 +46,11 @@ class TestFanCurve(unittest.TestCase):
             password="admin",
             add_wikidata_store=True
         )
-        # db.download_metadata()
-        # for file in db.rdf_directory.rglob("*.ttl"):
-        #     db.rdf_store.upload_file(file)
-        #     db.add_hdf_infile_index()
-        # db.add_hdf_infile_index()
+        db.download_metadata()
+        for file in db.rdf_directory.rglob("*.ttl"):
+            db.rdf_store.upload_file(file)
+            db.add_hdf_infile_index()
+        db.add_hdf_infile_index()
 
         # define the standard names:
         zenodo_record_ns = rdflib.namespace.Namespace("https://doi.org/10.5281/zenodo.17572275#")
@@ -86,7 +86,7 @@ class TestFanCurve(unittest.TestCase):
             n_scale
         )
         if n_rot == 600:
-            self.assertEqual(43, len(fan_curve))
+            self.assertEqual(41, len(fan_curve))
         elif n_rot == 1200:
             self.assertEqual(12, len(fan_curve))
 
